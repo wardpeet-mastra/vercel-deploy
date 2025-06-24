13a32d0bc81b325d1cb30fe52e4d667482e82057
wardpeet <ward@coding-tech.com>
2025-06-24 14:38:15 +0200

    add netlify

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,9 +1,10 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
-// import { LibSQLStore } from "@mastra/libsql";
+import { LibSQLStore } from "@mastra/libsql";
 import { weatherWorkflow } from "./workflows/weather-workflow";
 import { weatherAgent } from "./agents/weather-agent";
 import { VercelDeployer } from "@mastra/deployer-vercel";
+import { NetlifyDeployer } from "@mastra/deployer-netlify";
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
@@ -13,9 +14,7 @@ export const mastra = new Mastra({
   //   url: ":memory:",
   // }),
   // @ts-ignore
-  deployer: new VercelDeployer({
-    // token: "",
-  }),
+  deployer: new NetlifyDeployer(),
   logger: new PinoLogger({
     name: "Mastra",
     level: "info",
